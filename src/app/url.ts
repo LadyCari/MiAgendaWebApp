d5e2fd45fedfe2463ed747195389d2283df6c88a
changote <gomez.agustin1896@gmail.com>
2024-02-11 02:06:00 -0300

    borro caca

--- src/app/url.ts
+++
@@ -1,5 +0,0 @@
-export class Url{
-    public static urlItems: any = "http://localhost:3000/items"
-    public static urlConstantes: any = "http://localhost:3000/Constantes"
-    public static urlToDo: any = ""
-}
