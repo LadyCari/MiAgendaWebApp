c11385f02b020a6fcf28e264110526c3d9025741
Carolina Ferraris <k_roferraris@hotmail.com>
2024-02-13 00:40:05 -0300

    esperando get pj

--- agenda_front/src/app/Paginas/Juegos/BlackDessert/vista-personaje-bdo/vista-personaje-bdo.component.ts
+++ agenda_front/src/app/Paginas/Juegos/BlackDessert/vista-personaje-bdo/vista-personaje-bdo.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from 'src/app/Servicios/http.service';
+import { Url } from 'src/app/url';
 
 @Component({
   selector: 'app-vista-personaje-bdo',
   templateUrl: './vista-personaje-bdo.component.html',
   styleUrls: ['./vista-personaje-bdo.component.css']
 })
-export class VistaPersonajeBdoComponent {
 
+export class VistaPersonajeBdoComponent implements OnInit{
+
+readonly url = Url;
+
+constructor(private route: ActivatedRoute, private httpService: HttpService){
+}
+
+ngOnInit() {
+  // Recuperar la ID de la URL
+  const id = this.route.snapshot.paramMap.get('id');
+
+  // Utilizar la ID como necesites
+  console.log('ID recuperada de la URL:', id);
+}
+
+/*
+public informacionPj (id: number){
+  personaje = this.httpService.realizarGet(this.url. + id)
+}
+*/
 }
--- agenda_front/src/app/url.ts
+++ agenda_front/src/app/url.ts
@@ -5,4 +5,5 @@ export class Url{
     public static getPersonajesVistaPrevia = "http://localhost:8089/get-personajes-foto"
     public static getClasesPjs = "http://localhost:8089/get-clases"
     public static postNuevoPj = "http://localhost:8089/nuevo-personaje"
+    public static getInfoPersonaje = "http://localhost:8089/nuevo-personaje"
 }
